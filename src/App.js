cbbf8aa90885815f72bdc5f4bb345fa3d62dfe21
rafael <rafael.sergio@nextpage.com.br>
2019-08-07 11:10:22 -0300

    import component login and add some styles for application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Hellow World</h1>
+      <Login />
     </div>
   );
 }
